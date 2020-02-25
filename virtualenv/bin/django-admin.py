a8f3224010d72992e006c32db9b791634212720c
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-02-25 14:43:27 +0000

    project-setup

---
+++ virtualenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/arnoldumakhihe/other_clouds/OneDrive - University of Glasgow/python_workspace/explore-scotland/virtualenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
