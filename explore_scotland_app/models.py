f936032492d14b942719b516e2c87f8ed5ac22c7
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-02-29 08:46:32 +0000

    model started

--- explore_scotland_app/models.py
+++ explore_scotland_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+#import User
+
 # Create your models here.
+class Profile(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	profile_picture = models.ImageField(upload_to="")
+	
+	def __str__(self):
+		return self.user.first_name + self.user.last_name
+		
+class Photo(models.Model):
+	owner = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="photos_uploaded")
+	description = models.TextField(max_length=256)
+	
+	
