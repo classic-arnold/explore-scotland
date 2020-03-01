ada21fc8c4f253899d7de6cf6ddc92534592885b
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-03-01 19:33:12 +0000

    model complete

--- explore_scotland_app/models.py
+++ explore_scotland_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.core.exceptions import ValidationError
+
 #import User
 
 # Create your models here.
@@ -13,5 +15,30 @@ class Profile(models.Model):
 class Photo(models.Model):
 	owner = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="photos_uploaded")
 	description = models.TextField(max_length=256)
+	date_added = models.DateField(auto_now_add=True)
+	categories = models.ChoicesField()
+	tags = models.ChoicesField()
+	likes = models.ManyToManyField(Profile, related_name="photos_liked")
 	
+	
+	def __str__(self):
+		return self.owner.user.first_name + self.owner.user.last_name + "photo"
+		
+class Comment(models.Model):
+	owner = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="comment_posted")
+	content = models.TextField(max_length=256)
+	photo = models.ForeignKey(Photo, on_delete=models.CASCADE, related_name="photo_comments", null=True, blank=True)
+	comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name="comment_comments", null=True, blank=True)
+	date_added = models.DateField(auto_now_add=True)
+	
+	def clean(self):
+        super().clean()
+        if self.photo is None and self.comment is None:
+            raise ValidationError('Comments must be linked to either a photo or a comment.')
+	
+	def __str__(self):
+		if self.photo is not None:
+			return self.owner.user.first_name + self.owner.user.last_name + "comment on" + self.photo.__str__()
+		else:
+			return self.owner.user.first_name + self.owner.user.last_name + "comment on" + self.comment.__str__()
 	
