4820f7749c60646e47df37858113cc3343af0fdb
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-03-15 12:36:45 +0000

    cleaned

--- explore_scotland_app/views.py
+++ explore_scotland_app/views.py
@@ -9,111 +9,8 @@ from django.contrib.auth.decorators import login_required
 from datetime import datetime
 
 def index(request):
-    category_list = Category.objects.order_by('-likes')[:5]
-    page_list = Page.objects.order_by('-views')[:5]
-
-    context_dict = {}
-    context_dict['boldmessage'] = 'Welcome! Share & Love'
-    context_dict['categories']= category_list
-    context_dict['pages'] = page_list
-
-    # context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!', 'categories': category_list,
-    #               'pages': page_list, 'visits':request.session['visits']}
-
-    visitor_cookie_handler(request)
-    # context_dict['visits'] = request.session['visits']
-
-    # Obtain our Response object early so we can add cookie information.
-    response = render(request, 'pages/index.html', context=context_dict)
-
-    # Call the helper function to handle the cookies
-    # visitor_cookie_handler(request, response)
-    # request.session.set_test_cookie()
-    # Return response back to the user, updating any cookies that need changed.
-    return response
-
-#    return render(request, 'pages/index.html', context=context_dict)
-
-def about(request):
-    # prints out whether the method is a GET or a POST
-    print(request.method)
-    # prints out the user name, if no one is logged in it prints `AnonymousUser`
-    print(request.user)
-    # if request.session.test_cookie_worked():
-    #    print("TEST COOKIE WORKED!")
-    # request.session.delete_test_cookie()
-    context_dict = {}
-    visitor_cookie_handler(request)
-    context_dict['visits'] = request.session['visits']
-
-    # Obtain our Response object early so we can add cookie information.
-    response = render(request, 'pages/about.html', context=context_dict)
-    return response
-
-    # return render(request, 'pages/about.html') # context=context_dict) not needed
-
-def show_category(request, category_name_slug):
-    context_dict = {}
-    try:
-        category = Category.objects.get(slug=category_name_slug)
-        pages = Page.objects.filter(category=category)
-        context_dict['pages'] = pages
-        context_dict['category'] = category
-    except Category.DoesNotExist:
-        context_dict['category'] = None
-        context_dict['pages'] = None
-    return render(request, 'pages/category.html', context=context_dict)
-
-def add_category(request):
-    form = CategoryForm()
-    # A HTTP POST?
-    if request.method == 'POST':
-        form = CategoryForm(request.POST)
-        # Have we been provided with a valid form?
-        if form.is_valid():
-            # Save the new category to the database.
-            form.save(commit=True)
-            # print(cat, cat.slug()) # print reference to new category to the terminal
-            # Now that the category is saved, we could confirm this.
-            # For now, just redirect the user back to the index view.
-            return redirect('/explore_scotland_app/')
-
-        else:
-            # The supplied form contained errors -
-            # just print them to the terminal.
-            print(form.errors)
-
-    # Will handle the bad form, new form, or no form supplied cases.
-    # Render the form with error messages (if any).
-    return render(request, 'pages/add_category.html', {'form': form})
-
-def add_page(request, category_name_slug):
-    try:
-        category = Category.objects.get(slug=category_name_slug)
-    except Category.DoesNotExist:
-        category = None
-    if category is None:
-        return redirect('/explore_scotland_app/')
-    form = PageForm()
-    if request.method == 'POST':
-        form = PageForm(request.POST)
-        if form.is_valid():
-            if category:
-                page = form.save(commit=False)
-                page.category = category
-                page.views = 0
-                page.save()
-                # redirect the user to the show_category() view
-                return redirect(reverse('explore_scotland_app:show_category',
-                                kwargs={'category_name_slug':
-                                        category_name_slug}))
-        else:
-            print(form.errors)
-
-    context_dict = {'form': form, 'category': category}
-    # return render(request, 'pages/add_page.html', {'form': form})
-    return render(request, 'pages/add_page.html', context=context_dict)
-
+   return render(request, 'explore_scotland_app/homePage.html')
+   
 def register(request):
     # A boolean value for telling the template
     # whether the registration was successful.
@@ -213,10 +110,6 @@ def user_login(request):
         # blank dictionary object...
         return render(request, 'pages/login.html')
 
-@login_required
-def restricted(request):
-    return render(request, 'pages/restricted.html')
-
 # Use the login_required() decorator to ensure only those logged in can
 # access the view.
 @login_required
@@ -225,33 +118,3 @@ def user_logout(request):
     logout(request)
     # Take the user back to the homepage.
     return redirect(reverse('explore_scotland_app:index'))
-
-@login_required
-def user_return(request):
-    return redirect(reverse('explore_scotland_app:index'))
-
-def get_server_side_cookie(request, cookie, default_val=None):
-    val = request.session.get(cookie)
-    if not val:
-        val = default_val
-    return val
-
-def visitor_cookie_handler(request):
-    visits = int(get_server_side_cookie(request, 'visits', '1'))
-    last_visit_cookie = get_server_side_cookie(request,
-                                           'last_visit',
-                                           str(datetime.now()))
-    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
-                                    '%Y-%m-%d %H:%M:%S')
-
-    # If it's been more than a day since the last visit...
-    if (datetime.now() - last_visit_time).days > 0:
-        visits = visits + 1
-        # Update the last visit cookie now that we have updated the count
-        request.session['last_visit'] = str(datetime.now())
-    else:
-        # Set the last visit cookie
-        request.session['last_visit'] = last_visit_cookie
-
-    # Update/set the visits cookie
-    request.session['visits'] = visits
