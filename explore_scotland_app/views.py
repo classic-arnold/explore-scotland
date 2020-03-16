54673845d64210085a6ec11455a49cb943c43648
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-03-16 00:49:48 +0000

    profile

--- explore_scotland_app/views.py
+++ explore_scotland_app/views.py
@@ -102,9 +102,7 @@ def user_login(request):
 		# blank dictionary object...
 		return render(request, 'explore_scotland_app/login.html')
 
-# Use the login_required() decorator to ensure only those logged in can
-# access the view.
-@login_required
+
 def user_logout(request):
 	# Since we know the user is logged in, we can now just log them out.
 	logout(request)
