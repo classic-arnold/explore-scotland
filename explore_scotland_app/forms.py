0c3f7f955131ca40bc2a9a709d74dc5c3fac6568
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-03-16 16:19:34 +0000

    photos

--- explore_scotland_app/forms.py
+++ explore_scotland_app/forms.py
@@ -27,6 +27,10 @@ class PhotoForm(forms.ModelForm):
 		model = Photo
 		fields = ('picture', 'description', 'categories', 'tags')
 		
+	def __init__(self, *args, **kwargs):
+		super(PhotoForm, self).__init__(*args, **kwargs)
+		self.fields['tags'].widget.attrs['placeholder'] = 'Add tags separated by space'
+		
 class CommentForm(forms.ModelForm):
 	class Meta:
 		model = Comment
