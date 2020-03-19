7e6c3b1559f7f202677d3e60554f1de9914cc3b9
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-03-19 08:37:14 +0100

    decor

--- explore_scotland_app/models.py
+++ explore_scotland_app/models.py
@@ -42,33 +42,29 @@ class Photo(models.Model):
 	def __str__(self):
 		return self.owner.user.username + " photo."
 		
-	def save(self, *args, **kwargs):
-		super().save()
-		img = Image.open(self.picture.path)
-		width, height = img.size  # Get dimensions
-
-		if width > 300 and height > 300:
-			# keep ratio but shrink down
-			img.thumbnail((width, height))
-
-		# check which one is smaller
-		if height < width:
-			# make square by cutting off equal amounts left and right
-			left = (width - height) / 2
-			right = (width + height) / 2
-			top = 0
-			bottom = height
-			img = img.crop((left, top, right, bottom))
-
-		elif width < height:
-			# make square by cutting off bottom
-			left = 0
-			right = width
-			top = 0
-			bottom = width
-			img = img.crop((left, top, right, bottom))
-
-		img.save(self.picture.path)
+# 	def save(self, *args, **kwargs):
+# 		super().save()
+# 		img = Image.open(self.picture.path)
+# 		width, height = img.size  # Get dimensions
+# 
+# 		# check which one is smaller
+# 		if height < width:
+# 			# make square by cutting off equal amounts left and right
+# 			left = (width - height) / 2
+# 			right = (width + height) / 2
+# 			top = 0
+# 			bottom = height
+# 			img = img.crop((left, top, right, bottom))
+# 
+# 		elif width < height:
+# 			# make square by cutting off bottom
+# 			left = 0
+# 			right = width
+# 			top = 0
+# 			bottom = width
+# 			img = img.crop((left, top, right, bottom))
+# 
+# 		img.save(self.picture.path)
 		
 class Comment(models.Model):
 	owner = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="comment_posted")
