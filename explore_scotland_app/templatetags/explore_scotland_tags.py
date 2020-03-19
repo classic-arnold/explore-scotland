d7ff761a37e00a80a78f59ad567e428559ef7505
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-03-19 14:15:14 +0100

    decor

--- explore_scotland_app/templatetags/explore_scotland_tags.py
+++ explore_scotland_app/templatetags/explore_scotland_tags.py
@@ -7,3 +7,7 @@ register = template.Library()
 @register.inclusion_tag('explore_scotland_app/search-form.html')
 def get_search_form(category_searched=None, sorted_by=None):
 	return {'categories': Photo.CATEGORY_CHOICES, 'category_searched': category_searched, 'sorted_by': sorted_by}
+	
+@register.inclusion_tag('explore_scotland_app/photo-grid.html')
+def get_photo_grid(photos):
+	return {'photos': photos,}
