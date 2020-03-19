b015ab195ff435d260f1e875e57d4250c7832582
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-03-19 14:19:13 +0100

    decor

--- explore_scotland_app/templatetags/explore_scotland_tags.py
+++ explore_scotland_app/templatetags/explore_scotland_tags.py
@@ -5,8 +5,8 @@ from explore_scotland_app.models import Photo
 register = template.Library()
 
 @register.inclusion_tag('explore_scotland_app/search-form.html')
-def get_search_form(category_searched=None, sorted_by=None):
-	return {'categories': Photo.CATEGORY_CHOICES, 'category_searched': category_searched, 'sorted_by': sorted_by}
+def get_search_form(category_searched=None, sorted_by=None, query=None):
+	return {'categories': Photo.CATEGORY_CHOICES, 'category_searched': category_searched, 'sorted_by': sorted_by, 'query': query}
 	
 @register.inclusion_tag('explore_scotland_app/photo-grid.html')
 def get_photo_grid(photos):
