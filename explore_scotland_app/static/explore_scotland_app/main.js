57405f5137de8897ab3a2b2e14c82ea457a21a67
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-03-20 11:04:05 +0100

    overhaul

--- explore_scotland_app/static/explore_scotland_app/main.js
+++ explore_scotland_app/static/explore_scotland_app/main.js
@@ -7,8 +7,24 @@ $('document').ready(function(){
 	
 	$('input[type="file"]').addClass('p-1');
 	$('input[type="button"]').addClass('btn btn-primary');
+	
+	$("input[type=file]").change(function() {
+	  readURL(this);
+	});
 });
 
 function confirm_delete() {
   return confirm('Are you sure?');
+}
+
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    
+    reader.onload = function(e) {
+      $('#img_preview').attr('src', e.target.result);
+    }
+    
+    reader.readAsDataURL(input.files[0]);
+  }
 }
