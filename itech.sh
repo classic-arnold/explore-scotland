1a3de7875bddc79e4bcb1a29a09d9f93d2550e57
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-03-21 06:26:44 +0100

    social, deploy

--- itech.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /Users/arnoldumakhihe/other_clouds/OneDrive\ -\ University\ of\ Glasgow/python_workspace/explore-scotland
-source virtualenv/bin/activate
-python manage.py makemigrations
-python manage.py migrate
-python manage.py runserver
