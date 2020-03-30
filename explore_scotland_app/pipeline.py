649a3729d74c84a1d745cbb3f2421bbfd208fd0b
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-03-30 19:15:37 +0100

    social

---
+++ explore_scotland_app/pipeline.py
@@ -0,0 +1,21 @@
+from .models import UserProfile
+
+def create_profile(strategy, details, response, user, *args, **kwargs):
+	if UserProfile.objects.filter(user=user).exists():
+		pass
+	else:
+		new_profile = UserProfile(user=user)
+	
+		if backend.name == 'facebook':
+			res_id = response['id']
+			url = "http://graph.facebook.com/%s/picture?type=large"%res_id
+			ext = 'png'
+			img_name = str(user.pk)+'-image'
+			new_profile.picture.save(
+			   '{0}.{1}'.format(img_name, ext),
+			   ContentFile(urlopen(url).read()),
+			   save=False
+			)
+			new_profile.save()
+
+	return kwargs
