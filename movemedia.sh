d76a8445fdcee29f4abdb856ad7543a870bea6a6
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-03-31 22:19:48 +0100

    media change

---
+++ movemedia.sh
@@ -0,0 +1,6 @@
+mv media/test/Calton\ Hill.jpg media/test/calton_hill.jpg
+mv media/test/Queensferry-Crossing.png media/test/queensferry_crossing.png
+mv media/test/Every\ beautiful\ corner\ of\ Scotland.jpg media/test/scotland_beauty.jpg
+mv media/test/Scottish\ Piper.png media/test/scottish_piper.png
+mv media/test/Scottish\ sunrise.jpg media/test/scottish_sunrise.jpg
+mv media/test/Scottish\ yak.jpg media/test/scottish_yak.jpg
