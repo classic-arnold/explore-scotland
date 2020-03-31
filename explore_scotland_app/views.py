87849ae2c974297713d0c5497361b7b48f2b7408
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-03-31 15:47:08 +0100

    final v1

--- explore_scotland_app/views.py
+++ explore_scotland_app/views.py
@@ -153,7 +153,7 @@ def edit_profile(request):
 
 			# Update our variable to indicate that the template
 			# registration was successful.
-			messages.info(message='Update successful.', request=request)
+			messages.info(request, 'Update successful.')
 			return redirect(reverse('explore_scotland_app:index'))
 			
 	user_form = UserFormWithoutPassword(instance = request.user)
